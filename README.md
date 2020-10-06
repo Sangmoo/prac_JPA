41043c1fe4ba5558a34a2392a5d50dad58897db0
Sangmoo <first_1st@naver.com>
2020-10-07 00:18:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Practice SpringBoot JPA
+
+Dependencies
+
+- SpringBoot==2.1.16
+- spring-boot-starter-data-jpa
+- spring-boot-starter-thymeleaf
+- spring-boot-starter-web
+- Lombok
+- H2-DataBase
---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+
+		hello.setData("New Data");
+		String data = hello.getData();
+
+		System.out.println("data = " + data); // soutv - sysout - InteliJ
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
